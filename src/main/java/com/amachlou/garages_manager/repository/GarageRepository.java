c98cfdd926e040cef39116c96739390351eb0d9b
amachlou <abderrahim.machlou@gmail.com>
2025-10-13 19:14:51 +0100

    Added repositories

--- src/main/java/com/amachlou/garages_manager/repository/GarageRepository.java
+++ src/main/java/com/amachlou/garages_manager/repository/GarageRepository.java
@@ -1,4 +1,9 @@
 package com.amachlou.garages_manager.repository;
 
-public class GarageRepository {
+import com.amachlou.garages_manager.model.Garage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GarageRepository extends JpaRepository<Garage ,Long> {
 }
