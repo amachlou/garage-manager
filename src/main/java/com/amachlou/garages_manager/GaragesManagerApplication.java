015002f22d5335b69774e6e1566d2e00a9b9a41a
amachlou <abderrahim.machlou@gmail.com>
2025-10-13 19:17:44 +0100

    Commit Starter /test/ Class

---
+++ src/main/java/com/amachlou/garages_manager/GaragesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.amachlou.garages_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaragesManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amachlou/garages_manager/GaragesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amachlou.garages_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaragesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
