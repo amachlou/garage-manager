917907e9dee889366f1b11000c23ebf4106c0b8d
amachlou <abderrahim.machlou@gmail.com>
2025-10-13 19:13:39 +0100

    Added repositories

--- src/main/java/com/amachlou/garages_manager/repository/AccessoireRepository.java
+++ src/main/java/com/amachlou/garages_manager/repository/AccessoireRepository.java
@@ -1,8 +1,9 @@
 package com.amachlou.garages_manager.repository;
 
+import com.amachlou.garages_manager.model.Accessoire;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccessoireRepository extends JpaRepository {
+public interface AccessoireRepository extends JpaRepository<Accessoire, Long> {
 }
--- src/main/java/com/amachlou/garages_manager/repository/VehiculeRepository.java
+++ src/main/java/com/amachlou/garages_manager/repository/VehiculeRepository.java
@@ -1,9 +1,13 @@
 package com.amachlou.garages_manager.repository;
 
-import com.amachlou.garages_manager.model.Garage;
+import com.amachlou.garages_manager.model.Vehicule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface GarageRepository extends JpaRepository {
+public interface VehiculeRepository extends JpaRepository<Vehicule, Long> {
+    Optional<Vehicule> findByModel(String model);
 }
