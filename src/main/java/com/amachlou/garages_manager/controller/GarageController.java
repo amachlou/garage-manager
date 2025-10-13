9e858ad4be0364eb4e78f01b904aa82b6272fa01
amachlou <abderrahim.machlou@gmail.com>
2025-10-13 19:15:55 +0100

    Added Vehicule & Garage Controllers

--- src/main/java/com/amachlou/garages_manager/controller/GarageController.java
+++ src/main/java/com/amachlou/garages_manager/controller/GarageController.java
@@ -1,4 +1,87 @@
 package com.amachlou.garages_manager.controller;
 
+import com.amachlou.garages_manager.model.Garage;
+import com.amachlou.garages_manager.repository.GarageRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Book;
+import java.util.List;
+
+@RestController
+@RequestMapping("/garages")
+@AllArgsConstructor
 public class GarageController {
+//    Création, modification et suppression de garages.
+//    o Récupération d’un garage spécifique (par ID).
+//    o Liste paginée de tous les garages, avec possibilité de tri (par nom,ville, etc.).
+    private final GarageRepository garageRepository;
+
+    @PostMapping
+    public ResponseEntity<Garage> createGarage(@RequestBody Garage garage) {
+        Garage saved = garageRepository.save(garage);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+
+    @GetMapping
+    public Page<Garage> getAllGarages(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id,asc") String[] sort) {
+
+        // Parse sort params
+        String sortField = sort[0];
+        String sortDirection = sort.length > 1 ? sort[1] : "desc";
+
+        Sort.Direction direction = sortDirection.equalsIgnoreCase("desc") ? Sort.Direction.DESC : Sort.Direction.ASC;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortField));
+
+        return garageRepository.findAll(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Garage> getGarageById(@PathVariable Long id) {
+        return garageRepository.findById(id)
+                                .map(ResponseEntity::ok)
+                                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Garage> updateGarage(@PathVariable Long id, @RequestBody Garage updatedGarage) {
+        return garageRepository.findById(id).map(garage -> {
+            garage.setName(updatedGarage.getName());
+            garage.setAddress(updatedGarage.getAddress());
+            garage.setEmail(updatedGarage.getEmail());
+            garage.setVille(updatedGarage.getVille());
+            garage.setTelephone(updatedGarage.getTelephone());
+            garage.setVehicules(updatedGarage.getVehicules());
+
+            Garage saved = garageRepository.save(garage);
+
+            return ResponseEntity.ok(saved);
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Garage> deleteGarage(@PathVariable Long id) {
+        if (!garageRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        garageRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // SEARCH
+//    @GetMapping("/search")
+//    public ResponseEntity<List<GarageDocument>> searchGarages(@RequestParam(name = "q") String term) {
+//        List<GarageDocument> results = GarageSearchRepository.findByNameContainingOrDescriptionContaining(term, term);
+//        return ResponseEntity.ok(results);
+//    }
+
 }
--- src/main/java/com/amachlou/garages_manager/controller/VehiculeController.java
+++ src/main/java/com/amachlou/garages_manager/controller/VehiculeController.java
@@ -1,7 +1,7 @@
 package com.amachlou.garages_manager.controller;
 
-import com.amachlou.garages_manager.model.Garage;
-import com.amachlou.garages_manager.repository.GarageRepository;
+import com.amachlou.garages_manager.model.Vehicule;
+import com.amachlou.garages_manager.repository.VehiculeRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,26 +11,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Book;
-import java.util.List;
-
 @RestController
-@RequestMapping("/garages")
+@RequestMapping("/vehicules")
 @AllArgsConstructor
-public class GarageController {
-//    Création, modification et suppression de garages.
-//    o Récupération d’un garage spécifique (par ID).
-//    o Liste paginée de tous les garages, avec possibilité de tri (par nom,ville, etc.).
-    private final GarageRepository garageRepository;
+public class VehiculeController {
+
+//    * Ajout, modification et suppression de véhicules associés à un garage.
+//    o Lister les véhicules d’un garage spécifique.
+//    o Possibilité de lister tous les véhicules d’un modèle donné dans plusieurs vehicules.
+
+    private final VehiculeRepository vehiculeRepository;
 
     @PostMapping
-    public ResponseEntity<Garage> createGarage(@RequestBody Garage garage) {
-        Garage saved = garageRepository.save(garage);
+    public ResponseEntity<Vehicule> createVehicule(@RequestBody Vehicule vehicule) {
+        Vehicule saved = vehiculeRepository.save(vehicule);
         return ResponseEntity.status(HttpStatus.CREATED).body(saved);
     }
 
     @GetMapping
-    public Page<Garage> getAllGarages(
+    public Page<Vehicule> getAllVehicules(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
             @RequestParam(defaultValue = "id,asc") String[] sort) {
@@ -42,46 +41,46 @@ public class GarageController {
         Sort.Direction direction = sortDirection.equalsIgnoreCase("desc") ? Sort.Direction.DESC : Sort.Direction.ASC;
         Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortField));
 
-        return garageRepository.findAll(pageable);
+        return vehiculeRepository.findAll(pageable);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Garage> getGarageById(@PathVariable Long id) {
-        return garageRepository.findById(id)
-                                .map(ResponseEntity::ok)
-                                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<Vehicule> getVehiculeById(@PathVariable Long id) {
+        return vehiculeRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/modele/{model}")
+    public ResponseEntity<Vehicule> getVehiculeById(@PathVariable String model) {
+        return vehiculeRepository.findByModel(model)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Garage> updateGarage(@PathVariable Long id, @RequestBody Garage updatedGarage) {
-        return garageRepository.findById(id).map(garage -> {
-            garage.setName(updatedGarage.getName());
-            garage.setAddress(updatedGarage.getAddress());
-            garage.setEmail(updatedGarage.getEmail());
-            garage.setVille(updatedGarage.getVille());
-            garage.setTelephone(updatedGarage.getTelephone());
-            garage.setVehicules(updatedGarage.getVehicules());
+    public ResponseEntity<Vehicule> updateVehicule(@PathVariable Long id, @RequestBody Vehicule updatedVehicule) {
+        return vehiculeRepository.findById(id).map(vehicule -> {
+            vehicule.setBrand(updatedVehicule.getBrand());
+            vehicule.setModel(updatedVehicule.getType());
+            vehicule.setType(updatedVehicule.getType());
+            vehicule.setFuelType(updatedVehicule.getFuelType());
+            vehicule.setProductionYear(updatedVehicule.getProductionYear());
+            vehicule.setAccessoires(updatedVehicule.getAccessoires());
 
-            Garage saved = garageRepository.save(garage);
+            Vehicule saved = vehiculeRepository.save(vehicule);
 
             return ResponseEntity.ok(saved);
         }).orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Garage> deleteGarage(@PathVariable Long id) {
-        if (!garageRepository.existsById(id)) {
+    public ResponseEntity<Vehicule> deleteVehicule(@PathVariable Long id) {
+        if (!vehiculeRepository.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
-        garageRepository.deleteById(id);
+        vehiculeRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
-
-    // SEARCH
-//    @GetMapping("/search")
-//    public ResponseEntity<List<GarageDocument>> searchGarages(@RequestParam(name = "q") String term) {
-//        List<GarageDocument> results = GarageSearchRepository.findByNameContainingOrDescriptionContaining(term, term);
-//        return ResponseEntity.ok(results);
-//    }
 
 }
