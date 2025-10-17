e564e539e838300ad73f5c387fd3153464654ce3
amachlou <abderrahim.machlou@gmail.com>
2025-10-17 20:00:31 +0100

    optimize dataInitializer

--- src/main/java/com/amachlou/garages_manager/utils/DataInitializer.java
+++ src/main/java/com/amachlou/garages_manager/utils/DataInitializer.java
@@ -32,13 +32,17 @@ public class DataInitializer {
 			Garage g2 = garageRepository.save(new Garage(null,"Inzegan", "Bds Muqawama", "7009955", "dev@email.com","Agadir",null,null));
 			Garage g3 = garageRepository.save(new Garage(null,"Rabat", "Hay Riyad", "+092334455", "prod@email.com","Tanger",null,null));
 
-			Vehicule v1 = vehiculeRepository.save(new Vehicule(null,"MD01", "SEDAN", "Renault", Year.of(2020), "Diesel", g1, null));
-			Vehicule v2 = vehiculeRepository.save(new Vehicule(null,"MD02", "COPE", "BENZ", Year.of(2022), "Gaz", g2, null));
-			Vehicule v3 = vehiculeRepository.save(new Vehicule(null,"MD03", "CABRIOLET", "TESLA", Year.of(2023), "Gasoil", g3, null));
-
-			Accessoire a1 = accessoireRepository.save(new Accessoire(null,"Pignon", "15cm P", 76.8, "Originl", v1));
-			Accessoire a2 = accessoireRepository.save(new Accessoire(null,"Cable", "15cm P", 6.8, "CAT 2", v2));
-			Accessoire a3 = accessoireRepository.save(new Accessoire(null,"Cle", "15cm P", 00.8, "Super", v3));
+			Arrays.asList(g1,g2,g3).forEach(garage -> {
+				Arrays.asList(
+				vehiculeRepository.save(new Vehicule(null,"MD01", "SEDAN", "Renault", Year.of(2020), "Diesel", garage, null)),
+				vehiculeRepository.save(new Vehicule(null,"MD02", "COPE", "BENZ", Year.of(2022), "Gaz", garage, null)),
+				vehiculeRepository.save(new Vehicule(null,"MD03", "CABRIOLET", "TESLA", Year.of(2023), "Gasoil", garage, null)))
+						.forEach(vehicule -> {
+							accessoireRepository.save(new Accessoire(null,"Pignon", "15cm P", 76.8, "Originl", vehicule));
+							accessoireRepository.save(new Accessoire(null,"Cable", "15cm P", 6.8, "CAT 2", vehicule));
+							accessoireRepository.save(new Accessoire(null,"Cle", "15cm P", 00.8, "Super", vehicule));
+						});
+			});
 
 			OpeningHour oh1 = openingHourRepository.save(new OpeningHour(null, g1, DayOfWeek.MONDAY,null));
 			OpeningHour oh2 = openingHourRepository.save(new OpeningHour(null, g1, DayOfWeek.TUESDAY,null));
